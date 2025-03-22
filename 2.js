7d4a165a71e53c5590505fda6e9baefad00013d1
Rhythm-star <liumake0037@protonmail.com>
2025-03-22 16:36:28 +0800

    Delete 2.js

--- 2.js
+++
@@ -1,9 +0,0 @@
-<section>
-    <h2>我的照片</h2>
-    <img src="my-photo.jpg" alt="我的照片" width="300">
-</section>
-
-<section>
-    <h2>访问我的博客</h2>
-    <a href="https://my-blog.com" target="_blank">点击这里访问我的博客</a>
-</section>
